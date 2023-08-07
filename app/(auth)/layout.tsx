2a006a4c192b8b5326f9062ddad22ce6d2f74991
sonal <sonalsingh4102@gmail.com>
2023-08-07 20:31:39 +0530

    activity

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,7 +13,10 @@ export default function RootLayout({ children }: {children :React.ReactNode}){
    return (<ClerkProvider>
          <html lang="en">
             <body className={`${inter.className} bg-dark-1`}>
+               <div className="w-full flex justify-center items-center min-h-screen">
              {children}
+
+               </div>
             </body>
          </html>
    </ClerkProvider>)
